fbb756113f893237b2e0080dd636ebaccbe28d46
Snishant11 <snishant11@gmail.com>
2024-03-31 15:58:07 +0530

    using tanstack/reactquery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import Pet from './Pet'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import SearchParams from './SearchParams'
+import Details from './Details'
 
+const queryClinent = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+})
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <QueryClientProvider client={queryClinent}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
     </div>
   )
 }
--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -1,30 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
 import { useState, useEffect } from 'react'
+
+import fetchBreedList from './fetchBreedList'
 
 const localCache = {}
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([])
-  const [status, setStatus] = useState('unloaded')
-
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([])
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal])
-    } else {
-      requestBreedList()
-    }
-    async function requestBreedList() {
-      setBreedList([])
-      setStatus('loading')
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`,
-      )
-      const json = await res.json()
-
-      localCache[animal] = json.breeds || []
-      setBreedList(localCache[animal])
-      setStatus('Loaded')
-    }
-  }, [animal])
-  return [breedList, status]
+  const results = useQuery(['breeds', animal], fetchBreedList)
+  return [results?.data?.breeds ?? [], results.status]
 }
